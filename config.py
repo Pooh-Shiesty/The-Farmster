a61fc5a5f22630d63c0a59e455235be7ca59a638
Pepe The Roya <pimanroya06@gmail.com>
2021-08-23 18:44:10 +0200

    image recognition and config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+# Directories
+MELON_LOADER_C_DRIVE = "C:/Program Files (x86)/Steam/steamapps/common/BloonsTD6/version.dll"
+MELON_LOADER_D_DRIVE = "D:/SteamLibrary/steamapps/common/BloonsTD6/version.dll"
+BTD6_DIRECTORY_C_DRIVE = "C:/Program Files (x86)/Steam/steamapps/common/BloonsTD6/BloonsTD6.exe"
+BTD6_DIRECTORY_D_DRIVE = "D:/SteamLibrary/steamapps/common/BloonsTD6/BloonsTD6.exe"
+
+# Other
+FAST_MODE = False # Change if you want the script to run faster (You can get flagged if you change it to True)
+PAG_FAILSAFE = True # Only change if you know what you're doing!
+PAG_WAIT_TIME = 1
+PAG_START_SECONDS = 5
+CHECK_LEVEL_UP_COUNTER = 2
+
+# AUTO_LAUNCH_BTD6 = True
+
+OPENCV_CONFIDENCE = 0.85
+
+DEFLATION_BUTTON = "./resources/main_menu/deflation_mode.png"
+EASY_BUTTON = "./resources/main_menu/easy_difficulty.png"
+EXPERT_BUTTON = "./resources/main_menu/expert_button.png"
+INFERNAL_MAP_BUTTON = "./resources/main_menu/infernal_map.png"
+OK_BUTTON = "./resources/main_menu/ok_button.png"
+PLAY_BUTTON = "./resources/main_menu/play_button.png"
+START_BUTTON = "./resources/main_menu/start_button.png"
+START_GAME_BUTTON = "./resources/main_menu/button_game_start.jpg"
+FAST_FORWARD_BUTTON = "./resources/main_menu/button_game_fast_forward.jpg"
+PROMPT_LEVEL_UP = "./resources/main_menu/prompt_level_up.jpg"
+NEXT_BUTTON = "./resources/main_menu/next_button.png"
+
+# Hotkeys
+FAST_FORWARD = "space"
+NINJA_MONKEY = "d"
+ALCHEMIST_MONKEY = "f"
+
+# If buttons are wrong change to your upgrade buttons 
+UPGRADE_1 = "1"
+UPGRADE_2 = "2"
+UPGRADE_3 = "3"
--- main.py
+++ main.py
@@ -1,71 +1,73 @@
 import subprocess
 import time
 from pathlib import Path
-import pyautogui
+import pyautogui as pag
 import sys
 import numpy
+import config
 
-pyautogui.FAILSAFE = True
+pag.FAILSAFE = config.PAG_FAILSAFE
+# pag.PAUSE = config.PAG_WAIT_TIME
 
-print("You can drag your mouse to the top left corner of your screen if you want to")
-print("terminate the Python process or press Ctrl+C in the commandline")
+wait_location = None
+game_counter = 0
 
-# MelonLoader Locations
-cDriveModLibrary = "C:/Program Files (x86)/Steam/steamapps/common/BloonsTD6/version.dll"
-dDriveModLibrary = "D:/SteamLibrary/steamapps/common/BloonsTD6/version.dll"
-
-# BTD6 Locations
-cDriveLibrary = "C:/Program Files (x86)/Steam/steamapps/common/BloonsTD6/BloonsTD6.exe"
-dDriveLibrary = "D:/SteamLibrary/steamapps/common/BloonsTD6/BloonsTD6.exe"
+if config.PAG_FAILSAFE:
+    print("Drag your mouse to the top left corner of the screen to activate the Failsafe")
+else:
+    print("Warning: Failsafe is turned off!")
 
 # Check if MelonLoader is installed
-if Path(cDriveModLibrary).is_file():
-    btd6_directory = cDriveLibrary
-    print("MelonLoader is installed\nLaunching...")
-    print("Make sure you have Speedhack mod in your Mods folder!")
+if Path(config.MELON_LOADER_C_DRIVE).is_file():
+    btd6_directory = config.BTD6_DIRECTORY_C_DRIVE
 
-elif Path(dDriveModLibrary).is_file():
-    btd6_directory = dDriveLibrary
-    print("MelonLoader is installed\nLaunching...")
-    print("Make sure you have Speedhack mod in your Mods folder!")
+elif Path(config.MELON_LOADER_D_DRIVE).is_file():
+    btd6_directory = config.BTD6_DIRECTORY_D_DRIVE
 
 # And if MelonLoader isn't in default install locations, tell the user to input the directory
 else:
     print("Couldn't find MelonLoader")
     btd6_directory = input("Type in the path to your BTD6 directory where MelonLoader is installed (include BloonsTD6.exe in the end)")
     if Path(btd6_directory).is_file():
-        print("MelonLoader is installed\nLaunching...")
-        print("Make sure you have Speedhack mod in your Mods folder!")
+        pass
 
-# Launch BTD6 with Mods and wait 12.5 cause that's around how long it will take to launch
-# BTD6 on an average PC
+print("MelonLoader is installed\nLaunching...")
+print("Make sure you have Speedhack mod in your Mods folder!")
+
+def wait_location():
+    return wait_location
+
+def delay():
+    if config.FAST_MODE == True:
+        return numpy.random.uniform(0.075, 0.125)
+    else:
+        return numpy.random.uniform(0.1, 0.2)
+
+def click_image(img):
+    x = pag.locateCenterOnScreen(img, confidence=config.OPENCV_CONFIDENCE)
+    if x is None:
+        raise ValueError(f"Cannot find {x} on screen")
+    pag.click(x)
+
+# Launch BTD6
 subprocess.Popen(btd6_directory)
-time.sleep(14.25)
+time.sleep(17.5) # Wait just to make sure they in da menu
 
-#BTD6 Automatic Map Launch
-start_button = None
-print("Looking for Start Button...")
-while start_button == None:
-    if start_button != None:
-        print("Found Start Button")
-        break
-    start_button = pyautogui.locateCenterOnScreen("./resources/start_button.png", confidence=0.8)
-
-pyautogui.click(start_button)
+click_image(config.START_BUTTON)
 time.sleep(5)
-pyautogui.click(955, 716)
+click_image(config.OK_BUTTON)
 
 # XP, Monkey Money and Time Calculator
-counter = pyautogui.prompt(text="How many times would you like to complete this map? (Must type a whole number)", title="bruh")
-extra_mm_unlocked = pyautogui.confirm(text="Have you unlocked the Mo' Monkey Money Knowledge?", buttons=["Yes", "No"])
+counter = pag.prompt(text="How many times would you like to complete this map? (Must type a whole number)", title="bruh")
+extra_mm_unlocked = pag.confirm(text="Have you unlocked the Mo' Monkey Money Knowledge?", buttons=["Yes", "No"])
 if extra_mm_unlocked == "Yes":
     mk_m = int(counter) * 66
 else:
     mk_m = int(counter) * 60
 xp = int(counter) * float(59731)
 
-# HMTIT = How Much Time It Takes
-hmtit = 17.89 * float(counter)
+# HMTDIT = How Much Time Does It Takes
+hmtdit = 14.89 * float(counter)
 
 def convert(seconds):
     seconds = seconds % (24 * 3600)
@@ -77,7 +79,7 @@ def convert(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 print(f"Monkey Money: {mk_m}\nXP: {xp}")
-usure = pyautogui.confirm(text=f"Do you want to start farming? (Pressing no will terminate the process)\nEstimated Time: {convert(hmtit)}\nMonkey Money: {mk_m} | XP: {xp}", buttons=["Yes", "No"])
+usure = pag.confirm(text=f"Do you want to start farming? (Pressing no will terminate the process)\nEstimated Time: {convert(hmtdit)}\nMonkey Money: {mk_m} | XP: {xp}", buttons=["Yes", "No"])
 if usure == "Yes":
     print("Launching Infernal | Deflation Mode")
 
@@ -85,70 +87,61 @@ elif usure == "No":
     print("Terminating Python process! (BloonsTD6 will not be terminated)")
     sys.exit()
 
-# Random delay between 0.1 and 0.3 seconds
-delay = numpy.random.uniform(0.1, 0.3)
-
-n = 0
 while True:
-    if n == int(counter):
+    if game_counter == int(counter):
         break
-    n += 1
-    pyautogui.click(837, 938)
-    time.sleep(delay)
-
-    pyautogui.click(1340, 970)
-    time.sleep(delay)
-
-    pyautogui.click(517, 562)
-    time.sleep(delay)
-
-    pyautogui.click(618, 414)
-    time.sleep(delay)
-
-    pyautogui.click(1280, 466)
+    game_counter += 1
+    click_image(config.PLAY_BUTTON)
+    time.sleep(0.5)
+    click_image(config.EXPERT_BUTTON)
+    time.sleep(0.5)
+    click_image(config.INFERNAL_MAP_BUTTON)
+    time.sleep(0.5)
+    click_image(config.EASY_BUTTON)
+    time.sleep(0.5)
+    click_image(config.DEFLATION_BUTTON)
     time.sleep(5)
-
-    pyautogui.click(952, 765)
+    click_image(config.OK_BUTTON)
     time.sleep(1)
 
     # Deflation strat + loop
-    pyautogui.press("D")
-    pyautogui.moveTo(837, 366)
-    pyautogui.click(clicks=2, interval=0.2)
-    pyautogui.click(323, 490, clicks=4, interval=0.2)
-    pyautogui.click(340, 788, clicks=2, interval=0.2)
-    time.sleep(delay)
-    pyautogui.press("esc")
-    time.sleep(delay)
-    pyautogui.press("D")
-    pyautogui.moveTo(832, 699)
-    pyautogui.click(clicks=2, interval=0.2)
-    pyautogui.click(1585, 483, clicks=4, interval=0.2)
-    pyautogui.click(1542, 784, clicks=2, interval=0.2)
-    pyautogui.press("esc")
-    time.sleep(delay)
-    pyautogui.press("F")
-    pyautogui.moveTo(832, 766)
-    pyautogui.click(clicks=2, interval=0.2)
-    pyautogui.click(1585, 483, clicks=4, interval=0.2)
-    pyautogui.press("esc")
-    time.sleep(delay)
-    pyautogui.press("F")
-    pyautogui.moveTo(833, 290)
-    pyautogui.click(clicks=2, interval=0.2)
-    pyautogui.click(1585, 483, clicks=4, interval=0.2)
-    pyautogui.press("esc")
-    time.sleep(delay)
-    pyautogui.press("space")
-    time.sleep(delay)
-    pyautogui.press("space")
-    time.sleep(delay)
-    pyautogui.press("9")
+    pag.press(config.NINJA_MONKEY)
+    pag.moveTo(837, 366, duration=delay())
+    pag.click(clicks=2, interval=delay())
+    pag.click(323, 490, clicks=4, interval=delay())
+    pag.click(340, 788, clicks=2, interval=delay())
+    time.sleep(delay())
+    pag.press("esc")
+    time.sleep(delay())
+    pag.press(config.NINJA_MONKEY)
+    pag.moveTo(832, 699, duration=delay())
+    pag.click(clicks=2, interval=delay())
+    pag.click(1585, 483, clicks=4, interval=delay())
+    pag.click(1542, 784, clicks=2, interval=delay())
+    pag.press("esc")
+    time.sleep(delay())
+    pag.press(config.ALCHEMIST_MONKEY)
+    pag.moveTo(832, 766, duration=delay())
+    pag.click(clicks=2, interval=delay())
+    pag.click(1585, 483, clicks=4, interval=delay())
+    pag.press("esc")
+    time.sleep(delay())
+    pag.press(config.ALCHEMIST_MONKEY)
+    pag.moveTo(833, 290, duration=delay())
+    pag.click(clicks=2, interval=delay())
+    pag.click(1585, 483, clicks=4, interval=delay())
+    pag.press("esc")
+    time.sleep(delay())
+    pag.press("space")
+    time.sleep(delay())
+    pag.press("space")
+    time.sleep(delay())
+    pag.press("9")
     time.sleep(10)
-    pyautogui.click(945, 917)
-    time.sleep(1)
-    pyautogui.click(796, 857)
+    click_image(config.NEXT_BUTTON)
+    time.sleep(0.5)
+    pag.click(796, 857)
     time.sleep(3)
 
 # Alert the user in-case they didn't check the commandline and see if the program has finished
-pyautogui.alert(text=f"Done!\nYou earned {xp} XP and {mk_m} Monkey Money in {counter} run(s)!", title="Autofarm is done!", button="Let's GOOOOO!")
+pag.alert(text=f"Done!\nYou earned {xp} XP and {mk_m} Monkey Money in {counter} run(s)!", title="Autofarm is done!", button="Let's GOOOOO!")
